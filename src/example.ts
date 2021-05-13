edf41c2623c816283d48abe4f4769c4981c860ec
drvnk <eugenedrvnk@gmail.com>
2021-05-13 07:18:30 +0300

    add hometask w/o MyArray reduce

---
+++ src/example.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const concat = (startString: string, endString: string): string => {
+  return startString + endString;
+};
+
+type howIDoIt = string;
+
+interface myHometask {
+  howIDoIt: howIDoIt;
+  simeArray: [string, string, number];
+  withData: [{ howIDoIt: howIDoIt; simeArray: [string, number] }];
+}
+
+const myHometask: myHometask = {
+  howIDoIt: 'I Do It Wel',
+  simeArray: ['string one', 'string two', 42],
+  withData: [{ howIDoIt: 'I Do It Wel', simeArray: ['string one', 23] }],
+};
